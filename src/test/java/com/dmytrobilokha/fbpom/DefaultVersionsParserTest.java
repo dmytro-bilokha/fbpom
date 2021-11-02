1f6ddb1391d91d288f5ef6c585c4d7e37f241197
Dmytro Bilokha <dmytro@posteo.net>
2021-11-02 21:25:53 +0100

    Added test dependencies

---
+++ src/test/java/com/dmytrobilokha/fbpom/DefaultVersionsParserTest.java
@@ -0,0 +1,12 @@
+package com.dmytrobilokha.fbpom;
+
+import org.testng.annotations.Test;
+
+@Test(groups = "unit")
+public class DefaultVersionsParserTest {
+
+    public void recognizesValidVersionString() {
+
+    }
+
+}
